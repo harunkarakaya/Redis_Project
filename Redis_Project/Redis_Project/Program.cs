7aeff0d3ad25efd97408f82d4f9ac4733a21b5d9
Harun KARAKAYA <karakayaharun67@gmail.com>
2024-02-15 17:25:47 +0300

    project first commit

---
+++ Redis_Project/Redis_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redis_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
