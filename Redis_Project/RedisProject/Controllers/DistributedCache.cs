b4f2aff4e7e7d6fdc49830fed0539a8725f53988
Harun KARAKAYA <karakayaharun67@gmail.com>
2024-02-15 18:44:29 +0300

    setString

---
+++ Redis_Project/RedisProject/Controllers/DistributedCache.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace RedisProject.Controllers
+{
+    public class DistributedCache
+    {
+        IDistributedCache _distributedCache;
+
+        public DistributedCache(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public void CacheSetString()
+        {
+            //Metinsel türde key-value tarzında veri depolamasını gerçekleştiren metottur.
+            _distributedCache.SetString("date", DateTime.Now.ToString());
+        }
+
+        public void CacheSetString2() //Alternatifi
+        {
+            _distributedCache.SetString("date", DateTime.Now.ToString(), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(1200),
+                SlidingExpiration = TimeSpan.FromSeconds(60)
+            });
+        }
+    }
+}
