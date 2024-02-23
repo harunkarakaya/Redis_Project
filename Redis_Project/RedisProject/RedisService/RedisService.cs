1adc27a1122150091b55155481c58e484d9e2686
Harun KARAKAYA <karakayaharun67@gmail.com>
2024-02-23 18:51:28 +0300

    RedisService connectionMultiplexer

---
+++ Redis_Project/RedisProject/RedisService/RedisService.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+namespace RedisProject.RedisService
+{
+    public class RedisService
+    {
+        ConnectionMultiplexer connectionMultiplexer;
+        public void Connect() => connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379"); //redis sunucusuna bağlantı gerçekleştiriliyor.
+        public IDatabase GetDb(int db) => connectionMultiplexer.GetDatabase(db);
+    }
+}
